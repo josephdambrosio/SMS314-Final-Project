94011a15b2ffbf96a206188db48bfa1cadf0e7de
Joseph D'Ambrosio <58742052+josephdambrosio@users.noreply.github.com>
2022-03-28 10:37:49 -0400

    Update Final Project.R

--- Final Project.R
+++ Final Project.R
@@ -8,21 +8,21 @@ library(data.table)
 
 # START Dataset Selection and DF Import
 file.choose()
-SUPLdf <- fread("/Users/josephdambrosio/Dropbox/AA-Kutztown/2021/Spring/SMS Analytics/Final Project/SMS314 Final Project/DAMBROSIOSEEDCOV.csv")
+SUPLdf <- fread("/Users/josephdambrosio/Dropbox/AA-ACADEMICS/AA-Kutztown/2021/Spring/SMS Analytics/Final Project/SMS314 Final Project/DAMBROSIOSEEDCOV.csv")
 # END Dataset Selection and DF Import
 
 
 # START stage one (adv sentiment analysis)
 S1prep_df <- get_sentences(as.vector(SUPLdf$text))
-S1sent_df <- sentiment(prep_df)
+S1sent_df <- sentiment(S1prep_df)
 mean(S1sent_df$sentiment) 
 range(S1sent_df$sentiment)
-View(S1sent_df)
-z <- S1sent_df
+#View(S1sent_df)
+#z <- S1sent_df
 # END stage one (adv sentiment analysis)
 
 # START stage two (influence score)
-S2infl_df <- S1sent_df %>%
+S2infl_df <- SUPLdf %>%
   mutate(influence = from_user_followercount / from_user_friendcount)
 # END stage two (influence score)
 
@@ -35,3 +35,18 @@ S3recmb_df <- S1sent_df %>%
 # START stage four (sent scores and infl scores)
 S4newest_df <- data.frame(S2infl_df, S3recmb_df$'mean(sentiment)')
 # END stage four (sent scores and infl scores)
+
+# START stage five (clean up DF)
+S5clean_df <- S4newest_df %>%
+  filter(from_user_friendcount > 0, from_user_followercount > 0)
+S5finalClean_df <- S5clean_df %>%
+ mutate(sentiment = S3recmb_df..mean.sentiment..) %>%
+  select(-c(S3recmb_df..mean.sentiment..))
+  
+  
+  
+  
+  
+  
+  
+  
